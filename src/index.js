437a9176f98a9a670570fc6246c92dad1e708235
preciouslucas <preciouslucas@hotmail.com>
2021-01-07 17:45:04 +0000

    Added formatHours function for the forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
 
 let now = new Date();
 let h2 = document.querySelector("h2");
@@ -48,10 +78,7 @@ function formatHours(timestamp) {
   return `${hours}:${minutes}`;
 }
 
-function formatHours(timestamp) {
-  let date = new Date(timestamp);
-  return `${hours}:${minutes}`;
-}
+
 
 //Weather
 function showTemperature(response) {
